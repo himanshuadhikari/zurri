25c2ecb88bb719c95477c3eb52f41a6d648ea795
Himanshu Adhikari <himanshu.adhikari@tothenew.com>
2025-07-30 19:59:38 +0530

    fixed cookie issue

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -54,7 +54,7 @@ export async function POST(request: NextRequest) {
 
     response.cookies.set('auth_token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      // secure: process.env.NODE_ENV === 'production',
       path: '/',
       maxAge: 60 * 60 * 24, // 1 day
       sameSite: 'lax',
