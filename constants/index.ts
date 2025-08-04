5cca44a1ff95b8400d0254b63925c5355634b600
Himanshu Adhikari <himanshu.adhikari@tothenew.com>
2025-08-04 17:39:27 +0530

    added file for home page seo

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,31 @@
+
+
+export const homePageMetaData = {
+    title: "Buy Premium Cordsets Online in India | Zurri",
+    description:
+        "Shop premium quality cordsets at Zurri – India's trusted online store. Durable, stylish, and affordable cordsets with fast delivery across India.",
+    keywords:
+        "cordset, cordsets India, buy cordset online, premium cordset, electric cordsets, stylish cordsets",
+    openGraph: {
+        title: "Buy Premium Cordsets Online in India | Zurri",
+        description:
+            "Explore a wide range of high-quality cordsets at affordable prices. Perfect for home, office, parties or industry use.",
+        url: "https://www.zurri.in/",
+        type: "website",
+        images: [
+            {
+                url: "https://res.cloudinary.com/dauxlircm/image/upload/v1754307866/WhatsApp_Image_2025-07-22_at_22.28.13_gahelr_c_pad_w_1200_h_630_bofvya.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Cordset Product Image",
+            },
+        ],
+    },
+};
+
 const defaultContants = {
     "RUPEE_SIGN": "₹"
 }
 
 export default defaultContants
+
